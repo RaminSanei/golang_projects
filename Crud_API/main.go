90e72f1cceeeb545efc0626692212a411129f60d
Ramin  Sanei <raminsanei1992@gmail.com>
2025-05-22 16:03:01 +0000

    Add main function & submethods .

--- Crud_API/main.go
+++ Crud_API/main.go
@@ -22,3 +22,20 @@ type Director struct {
 }
 
 var movies []movies
+
+
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "438-1234567890", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "438-0987654321", Title: "Movie Two", Director: &Director{Firstname: "Jane", Lastname: "Smith"}})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Println("Server started on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
